6e3e8e75dce732c618d43e9a9edcde8e9c2bf2e8
xxxelf <32174315+xxxelf@users.noreply.github.com>
2017-12-06 14:56:55 +0100

    created routes

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
+var dotenv = require('dotenv')
+dotenv.config();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+/*ROUTES*/
 var index = require('./routes/index');
-var users = require('./routes/users');
+var board = require('./routes/board');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URI, {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE,
+  useMongoClient: true
+});
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,24 +29,23 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/board', board);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res) => {
+  res.status(404);
+  res.json({ error: "Not Found" });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  // allways log the error
+  console.error("ERROR", req.method, req.path, err);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // only send response if the error
+  if (!res.headerSent) {
+    res.status(500);
+    res.json({ error: "error.unexpected" });
+  }
 });
 
 module.exports = app;
---
+++ routes/board.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+
+var Board = require("../models/board-model").Board;
+
+/* GET ALL BOARDS. */
+router.get('/', function(req, res, next) {
+    res.status(200).json({ boards: "list all boards" });
+});
+
+/* GET ONE BOARD. */
+router.get('/:boardPath', function(req, res, next) {
+    res.status(200).json({ board: "show one board" });
+});
+
+/* GET ALL THREADS OF ONE BOARD. */
+router.get('/:boardPath/thread', function(req, res, next) {
+    res.status(200).json({ threads: "list all threads" });
+});
+
+/* GET ONE THREAD. */
+router.get('/:boardPath/thread/:threadNo', function(req, res, next) {
+    res.status(200).json({ threads: "show one thread" });
+});
+
+/* GET ALL REPLIES. */
+router.get('/:boardPath/thread/:threadNo/reply', function(req, res, next) {
+    res.status(200).json({ threads: "list all replies" });
+});
+
+/* GET ONE REPLY. */
+router.get('/:boardPath/thread/:threadNo/reply/:replyNo', function(req, res, next) {
+    res.status(200).json({ threads: "show one reply" });
+});
+
+/* POST ONE THREAD. */
+router.post('/:boardPath/thread/', function(req, res, next) {
+    res.status(200).json({ message: "success! Saved thread" });
+});
+
+/* POST ONE REPLY. */
+router.post('/:boardPath/thread/:threadNo/reply', function(req, res, next) {
+    res.status(200).json({ message: "success! Saved reply" });
+});
+
+module.exports = router;
